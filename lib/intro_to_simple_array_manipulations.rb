4798805b929649feccb0fb6b027fd9db2dba4038
Olivia Kasten <odkasten@gmail.com>
2018-09-16 14:45:25 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  array.push(string)
+end 
+
+def using_unshift(array, string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop 
+end 
+
+def pop_with_args(array)
+  popped = []
+  2.times do 
+    popped << array.pop
+  end 
+end 
